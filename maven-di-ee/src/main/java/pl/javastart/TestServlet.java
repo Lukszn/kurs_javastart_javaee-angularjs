31938d11b13eae0ad85ddd015b6f1e7ca10bc8b3
Lukas Sznurowski <lukaszsznurowski@gmail.com>
2018-07-05 17:50:02 +0200

    testing CDI in Java EE with servlet

---
+++ maven-di-ee/src/main/java/pl/javastart/TestServlet.java
@@ -0,0 +1,25 @@
+package pl.javastart;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+    
+	private static final long serialVersionUID = 1L;
+    
+    @Inject
+    private SimpleBean messageProducer;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println(messageProducer.getMessage());
+    }
+
+}
