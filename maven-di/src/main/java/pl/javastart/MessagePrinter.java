4b72b39332a495bc2a246bc7ef7d8a313772dbd7
Lukas Sznurowski <lukaszsznurowski@gmail.com>
2018-07-06 12:51:54 +0200

    injection by constructor for better testing np. Mock

--- maven-di/src/main/java/pl/javastart/MessagePrinter.java
+++ maven-di/src/main/java/pl/javastart/MessagePrinter.java
@@ -3,12 +3,16 @@ package pl.javastart;
 import javax.inject.Inject;
 
 public class MessagePrinter {
-	
-	@Inject
-	private MessageProducer messageProducer;
-	
-	public void printMessage() {
-		String message = messageProducer.getMessage();
-		System.out.println(message);
-	}
+
+    private MessageProducer messageProducer;
+
+    @Inject
+    public MessagePrinter(MessageProducer messageProducer) {
+        this.messageProducer = messageProducer;
+    }
+
+    public void printMessage() {
+        String message = messageProducer.getMessage();
+        System.out.println(message);
+    }
 }
