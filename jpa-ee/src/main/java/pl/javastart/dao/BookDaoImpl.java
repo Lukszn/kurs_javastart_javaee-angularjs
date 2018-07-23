c022017c02367368bdd164061b0ff2f486bf8e7c
Lukas Sznurowski <lukaszsznurowski@gmail.com>
2018-07-23 12:41:12 +0200

    simply configuraion and model with servlet

---
+++ jpa-ee/src/main/java/pl/javastart/dao/BookDaoImpl.java
@@ -0,0 +1,34 @@
+package pl.javastart.dao;
+
+import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceUnit;
+
+import pl.javastart.model.Book;
+
+@RequestScoped
+public class BookDaoImpl implements BookDao {
+
+    @PersistenceUnit(name = "jpa-ee")
+    private EntityManagerFactory emFactory;
+
+    @Override
+    public void save(Book book) {
+        EntityManager entityManager = emFactory.createEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        entityManager.persist(book);
+        tx.commit();
+        entityManager.close();
+    }
+
+    @Override
+    public Book get(Long id) {
+        EntityManager entityManager = emFactory.createEntityManager();
+        Book book = entityManager.find(Book.class, id);
+        entityManager.close();
+        return book;
+    }
+}
---
+++ jpa-ee/src/main/java/pl/javastart/servlet/GetBook.java
@@ -0,0 +1,29 @@
+package pl.javastart.servlet;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pl.javastart.dao.BookDao;
+import pl.javastart.model.Book;
+
+@WebServlet("/get")
+public class GetBook extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+    @Inject
+    private BookDao bookDao;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String bookId = request.getParameter("id");
+        Long id = Long.valueOf(bookId);
+        Book book = bookDao.get(id);
+        response.getWriter().println(book);
+    }
+}
